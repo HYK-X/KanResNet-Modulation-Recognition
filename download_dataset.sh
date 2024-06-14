2147f4e54248579a39cfd6af13557f1e4e5eb743
HYK-X <62224761+HYK-X@users.noreply.github.com>
2024-06-14 22:18:54 +0800

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,4 +2,4 @@ mkdir data
 cd data
 wget -O RML2016.10a.tar.bz2 "https://opendata.deepsig.io/datasets/2016.10/RML2016.10a.tar.bz2"
 tar xvjf RML2016.10a.tar.bz2
-rm RML2016.10b.tar.bz2
+rm RML2016.10a.tar.bz2
